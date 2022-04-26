ad791e87c26f3cfce9f3661e3ca30fa26a843e73
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-04-26 21:36:40 +0700

    Initial commit

---
+++ CuaHangDoCauCa/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CuaHangDoCauCa.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CuaHangDoCauCa/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CuaHangDoCauCa.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
