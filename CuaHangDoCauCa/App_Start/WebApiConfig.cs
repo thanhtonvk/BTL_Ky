7d6284893a5d413eead0925f21bd1b2b59da8adc
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-04-26 23:00:59 +0700

    update date api

--- CuaHangDoCauCa/App_Start/WebApiConfig.cs
+++ CuaHangDoCauCa/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace CuaHangDoCauCa
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ CuaHangDoCauCa/Utils/Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CuaHangDoCauCa.Utils
+{
+    public class Common
+    {
+        public static string baseUrl = "https://localhost:44315/";
+    }
+}
